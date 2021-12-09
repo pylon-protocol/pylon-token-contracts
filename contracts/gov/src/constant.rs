a617b8f4ea4616a50ef9e5124d6384c4b1fd6ad7
frostornge <frostornge@gmail.com>
2021-12-09 16:06:19 +0900

    feat(gov): increase query limit

--- contracts/gov/src/constant.rs
+++ contracts/gov/src/constant.rs
@@ -2,5 +2,5 @@ pub const API_VERSION: &str = "1.1.0";
 pub const POLL_EXECUTE_REPLY_ID: u64 = 1;
 
 // pagination
-pub const MAX_QUERY_LIMIT: u32 = 30;
-pub const DEFAULT_QUERY_LIMIT: u32 = 10;
+pub const MAX_QUERY_LIMIT: u32 = 100;
+pub const DEFAULT_QUERY_LIMIT: u32 = 50;
